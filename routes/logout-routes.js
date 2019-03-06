70176522ef98f3df9da015315f09fc29048ef122
Ender <cbessjr@gmail.com>
2019-03-05 19:00:51 -0700

    Resetting the project to implement React

--- routes/logout-routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-  //POST Login authentication route (uses Passport)
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
-};
