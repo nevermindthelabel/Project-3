9228d1348a373a9e8ecf5cc1813cb2dcef5cdd0c
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-11 20:32:39 -0700

    fixed navbar

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Navbar, { Modal } from "../components/Navbar";
+
+class Home extends Component {
+	state = {}
+
+	//functions n stuff
+
+	render() {
+		<Navbar>
+			{/* blah blah blah */}
+			{/* <Modal /> */}
+		</Navbar>
+	}
+}
