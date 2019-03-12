9ea95e611772a8e77c8c2aefe4737310c8ad4597
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-11 20:40:30 -0700

    fixed new user form

--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -2,45 +2,24 @@ import React from 'react';
 
 
 
-class NewUserForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('User Name and Password Created: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            User Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
+export function NewUserForm(props) {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <label>
+        User Name:
+            <input type="text" value={props.state.value} onChange={props.handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
 
-          <label>
-            Password:
-            <input type="password" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
+      <label>
+        Password:
+            <input type="password" value={props.state.value} onChange={props.handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
 
-          <button variant="primary" size="sm">
-            Submit
+      <button variant="primary" size="sm">
+        Submit
           </button>
-        </form>
-      );
-    }
-  }
-
-  export default NewUserForm;
+    </form>
+  );
+}
