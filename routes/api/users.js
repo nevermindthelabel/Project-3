6c004b497a19ef390f0147180882e2087171bd87
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-11 19:38:09 -0700

    maybe fixed passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 const isAuthenticated = require("../../config/middleware/isAuthenticated.js");
+var passport = require("../../config/passport");
+
 
 //Matches with "/api/users"
 router.route("/")
@@ -11,10 +13,15 @@ router.route("/:id")
 	.delete(usersController.remove);
 
 //Matches with "/api/users/current-user"
-router.route("/current-user")
+router.route("/current-user", isAuthenticated)
 	.get((req, res) => {
 		console.log(req.user);
 		res.json(req.user);
 	});
 
+//Matches with "/api/users/login"
+router.route("/login")
+	.post(passport.authenticate("local"), function (req, res) {
+		res.redirect("/");
+	})
 module.exports = router
