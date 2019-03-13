f6291163f4488dbe49c0f1d03c1bad6bede466d5
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-12 17:36:59 -0700

    added newreport page and new report form component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import NewReport from "./pages/NewReport";
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/new-report" component={NewReport} />
         </Switch>
       </Router>
     );
---
+++ client/src/pages/NewReport.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import NewReportForm from "../components/NewReportForm";
+
+class NewReport extends Component {
+	state = {
+
+	};
+
+	render() {
+		return (
+			<NewReportForm /> 
+		)
+	}
+}
+
+export default NewReport;
