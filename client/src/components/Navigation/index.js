ea963a9ddffe94543e906e53c8c6559c0929e98d
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-03-17 22:32:57 -0700

    added moment.js need to run npm install before running added current date/time to navbar

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -3,14 +3,22 @@ import "./Nav.css";
 import { Nav, Button, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import API from "../../utils/API";
+var moment = require('moment');
+
+let dateAndTime;
 
 export default class Navigation extends React.Component {
     logoutClick = event => {
         event.preventDefault();
         API.users.logout();
     }
+    getDateTime = () => {
+        dateAndTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        console.log(dateAndTime);
+    }
 
     render() {
+        this.getDateTime();
         return (
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Navbar.Brand href="#home">TrafficMon</Navbar.Brand>
@@ -25,6 +33,7 @@ export default class Navigation extends React.Component {
                         <Button variant="Login"><Link to={"/login"}><strong>Login</strong></Link></Button>
                         <Button variant="Logout" onClick={this.logoutClick}><strong>Logout</strong></Button>
                     </Nav>
+                    <Nav.Link as="strong"><strong>{dateAndTime}</strong></Nav.Link>
                 </Navbar.Collapse>
             </Navbar>
         )
