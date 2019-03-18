fcbd27aec365db644f2082dfc3d277d9a04bb840
james barker <jamesbarker585@gmail.com>
2019-03-17 18:23:04 -0700

    chane to login

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Nav.css";
-import LoginDialog from '../LoginDialog'
 import { Nav, Button, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
