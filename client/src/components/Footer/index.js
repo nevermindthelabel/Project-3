2b8493380011c29302888ac097a9a3166309c9ba
Matt Kilcup <nevermindthelabel@gmail.com>
2019-04-27 16:18:29 -0700

    added space inbetween users

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,10 +5,9 @@ export default function(props) {
   return (
     <footer>
       <p className="text-center">
-        Developed By <a href="https://github.com/cbessjr">Charles Bess</a>,
-        <a href="https://github.com/BrenKnigh">Brenden Knight</a>,
-        <a href="https://github.com/jamesbarker585">James Barker</a>,
-        <a href="https://github.com/nevermindthelabel">Matt Kilcup</a>,
+        Developed By <a href="https://github.com/cbessjr">Charles Bess</a>,{' '}
+        <a href="https://github.com/jamesbarker585">James Barker</a>,{' '}
+        <a href="https://github.com/nevermindthelabel">Matt Kilcup</a>,{' '}
         <a href="https://github.com/ghost0fharambe">Adam Karman</a>
       </p>
     </footer>
