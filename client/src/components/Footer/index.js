b3789d6c3fb8e42a7187cf72f93a6708618b3b7f
Matt Kilcup <nevermindthelabel@gmail.com>
2019-05-05 22:26:15 -0700

    Update to names

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,8 +7,8 @@ export default function(props) {
       <p className="text-center">
         Developed By <a href="https://github.com/cbessjr">Charles Bess</a>,{' '}
         <a href="https://github.com/jamesbarker585">James Barker</a>,{' '}
-        <a href="https://github.com/nevermindthelabel">Matt Kilcup</a>,{' '}
-        <a href="https://github.com/ghost0fharambe">Adam Karman</a>
+        <a href="https://github.com/ghost0fharambe">Adam Karman</a>, {' '}
+        and <a href="https://github.com/nevermindthelabel">Matt Kilcup</a>
       </p>
     </footer>
   );
